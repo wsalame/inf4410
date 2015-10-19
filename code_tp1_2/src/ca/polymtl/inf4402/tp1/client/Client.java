a54337eab09ccdc091a6df400fffff4dac8cc12d
wsalame <wsalame89@gmail.com>
2015-10-19 08:36:03 -0400

    commentaires

--- code_tp1_2/src/ca/polymtl/inf4402/tp1/client/Client.java
+++ code_tp1_2/src/ca/polymtl/inf4402/tp1/client/Client.java
@@ -14,206 +14,204 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.security.NoSuchAlgorithmException;
-
 import ca.polymtl.inf4402.tp1.shared.ServerInterface;
 import ca.polymtl.inf4402.tp1.shared.ServerInterface.AllowedCommand;
 import ca.polymtl.inf4402.tp1.shared.Utils;
 
 public class Client {
 
-	private final String CLIENT_ID_FILENAME = "clientId.txt";
+  private final String CLIENT_ID_FILENAME = "clientId.txt";
 
-	private ServerInterface localServerStub = null;
+  private ServerInterface localServerStub = null;
 
-	public static void main(String[] args) {
-		try {
-			if (args.length == 0 || args.length > 2) {
-				throw new IllegalArgumentException("Veuillez rentrez un nombre de parametres valides");
-			}
+  public static void main(String[] args) {
+    try {
+      if (args.length == 0 || args.length > 2) {
+        throw new IllegalArgumentException("Veuillez rentrez un nombre de parametres valides");
+      }
 
-			AllowedCommand command = AllowedCommand.valueOfCustom(args[0]);
+      AllowedCommand command = AllowedCommand.valueOfCustom(args[0]);
 
-			if (command == null) {
-				throw new IllegalArgumentException("Veuillez rentrez une commande valide");
-			}
+      if (command == null) {
+        throw new IllegalArgumentException("Veuillez rentrez une commande valide");
+      }
 
-			String filename = null;
+      String filename = null;
 
-			if (args.length > 1) {
-				filename = args[1];
-			}
+      if (args.length > 1) {
+        filename = args[1];
+      }
 
-			Client client = new Client();
-			client.run(command, filename);
-		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-		} catch (NoSuchAlgorithmException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+      Client client = new Client();
+      client.run(command, filename);
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+    } catch (NoSuchAlgorithmException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 
-	public Client() {
-		super();
+  public Client() {
+    super();
 
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
-		}
+    if (System.getSecurityManager() == null) {
+      System.setSecurityManager(new SecurityManager());
+    }
 
-		localServerStub = loadServerStub("127.0.0.1");
-	}
+    localServerStub = loadServerStub("127.0.0.1");
+  }
 
-	private void run(AllowedCommand command, String filename) throws NoSuchAlgorithmException {
-		try {
-			String clientId = hasClientId() ? getClientIdFromLocal()
-					: generateClientIdAndSave(localServerStub.generateClientId());
+  private void run(AllowedCommand command, String filename) throws NoSuchAlgorithmException {
+    try {
+      String clientId = hasClientId() ? getClientIdFromLocal() : saveClientId(localServerStub.generateClientId());
 
-			String checksum = filename != null ? getChecksum(filename) : null;
+      String checksum = filename != null ? calculateChecksum(filename) : null;
 
-			switch (command) {
-			case CREATE:
-				System.out.println(localServerStub.create(filename));
-				break;
-			case GET:
-				get(filename);
-				System.out.println(filename + " à été synchronisé");
-				break;
-			case LIST:
-				System.out.println(localServerStub.list());
-				break;
-			case LOCK:
-				byte[] potentialNewData = localServerStub.lock(filename, clientId, checksum);
+      switch (command) {
+      case CREATE:
+        System.out.println(localServerStub.create(filename));
+        break;
+      case GET:
+        get(filename);
+        System.out.println(filename + " à été synchronisé");
+        break;
+      case LIST:
+        System.out.println(localServerStub.list());
+        break;
+      case LOCK:
+        byte[] potentialNewData = localServerStub.lock(filename, clientId, checksum);
 
-				if (potentialNewData != null) {
-					System.out.println("NEW DATA");
-					saveFileToLocal(filename, potentialNewData);
-				}
+        if (potentialNewData != null) {
+          System.out.println("NEW DATA");
+          saveFileToLocal(filename, potentialNewData);
+        }
 
-				System.out.println(filename + " à été verrouillé");
-				break;
-			case PUSH:
-				byte[] data = getDataFromLocal(filename);
-				System.out.println(localServerStub.push(filename, data, clientId));
-				break;
-			case SYNC_LOCAL_DIR:
-				int countSyncedFiles = syncLocalDir();
-				System.out.println(countSyncedFiles + " fichier(s) ont été téléchargés");
-				break;
-			}
-		} catch (RemoteException e) {
-			System.out.println("Erreur avec le serveur");
-		}
+        System.out.println(filename + " à été verrouillé");
+        break;
+      case PUSH:
+        byte[] data = getDataFromLocal(filename);
+        System.out.println(localServerStub.push(filename, data, clientId));
+        break;
+      case SYNC_LOCAL_DIR:
+        int countSyncedFiles = syncLocalDir();
+        System.out.println(countSyncedFiles + " fichier(s) ont été téléchargés");
+        break;
+      }
+    } catch (RemoteException e) {
+      System.out.println("Erreur avec le serveur");
+    }
 
-	}
+  }
 
-	private int syncLocalDir() throws RemoteException {
-		String[][] files = localServerStub.syncLocalDir();
+  private int syncLocalDir() throws RemoteException {
+    String[][] files = localServerStub.syncLocalDir();
 
-		// Extracting the data
-		for (int i = 0; i < files.length; i++) {
-			String filename = files[i][0];
-			byte[] data = files[i][1].getBytes();
-			saveFileToLocal(filename, data);
-			System.out.println(filename);
-		}
+    // Extracting the data
+    for (int i = 0; i < files.length; i++) {
+      String filename = files[i][0];
+      byte[] data = files[i][1].getBytes();
+      saveFileToLocal(filename, data);
+      System.out.println(filename);
+    }
 
-		int numberOfFiles = files.length;
-		return numberOfFiles;
-	}
+    int numberOfFiles = files.length;
+    return numberOfFiles;
+  }
 
-	private void get(String filename) throws RemoteException, NoSuchAlgorithmException {
-		String checksum = getChecksum(filename);
-		byte[] remoteData = localServerStub.get(filename, checksum);
-		if (remoteData != null) {
-			saveFileToLocal(filename, remoteData);
-		}
-	}
+  private void get(String filename) throws RemoteException, NoSuchAlgorithmException {
+    String checksum = calculateChecksum(filename);
+    byte[] remoteData = localServerStub.get(filename, checksum);
+    if (remoteData != null) {
+      saveFileToLocal(filename, remoteData);
+    }
+  }
 
-	private ServerInterface loadServerStub(String hostname) {
-		ServerInterface stub = null;
+  private ServerInterface loadServerStub(String hostname) {
+    ServerInterface stub = null;
 
-		try {
-			Registry registry = LocateRegistry.getRegistry(hostname);
-			stub = (ServerInterface) registry.lookup("server");
-		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-		} catch (NotBoundException e) {
-			System.out.println("Erreur: Le nom '" + e.getMessage() + "' n'est pas défini dans le registre.");
-		} catch (AccessException e) {
-			System.out.println("Erreur: " + e.getMessage());
-		} catch (RemoteException e) {
-			System.out.println("Erreur: " + e.getMessage());
-		}
+    try {
+      Registry registry = LocateRegistry.getRegistry(hostname);
+      stub = (ServerInterface) registry.lookup("server");
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+    } catch (NotBoundException e) {
+      System.out.println("Erreur: Le nom '" + e.getMessage() + "' n'est pas défini dans le registre.");
+    } catch (AccessException e) {
+      System.out.println("Erreur: " + e.getMessage());
+    } catch (RemoteException e) {
+      System.out.println("Erreur: " + e.getMessage());
+    }
 
-		return stub;
-	}
+    return stub;
+  }
 
-	private String generateClientIdAndSave(String clientId) {
-		try {
-			PrintWriter out = new PrintWriter(CLIENT_ID_FILENAME);
-			out.print(clientId);
-			out.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+  private String saveClientId(String clientId) {
+    try {
+      PrintWriter out = new PrintWriter(CLIENT_ID_FILENAME);
+      out.print(clientId);
+      out.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
 
-		return clientId;
-	}
+    return clientId;
+  }
 
-	private byte[] getDataFromLocal(String filename) {
-		byte[] dataEncoded = null;
-		try {
-			Path path = Paths.get(filename);
-			if (path != null && path.toFile().exists()) {
-				dataEncoded = Files.readAllBytes(path);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+  private byte[] getDataFromLocal(String filename) {
+    byte[] dataEncoded = null;
+    try {
+      Path path = Paths.get(filename);
+      if (path != null && path.toFile().exists()) {
+        dataEncoded = Files.readAllBytes(path);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
-		return dataEncoded;
-	}
+    return dataEncoded;
+  }
 
-	private void saveFileToLocal(String filename, byte[] newData) {
-		PrintWriter out;
-		try {
-			out = new PrintWriter(filename);
-			out.print(new String(newData, "UTF-8"));
-			out.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
+  private void saveFileToLocal(String filename, byte[] newData) {
+    PrintWriter out;
+    try {
+      out = new PrintWriter(filename);
+      out.print(new String(newData, "UTF-8"));
+      out.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+  }
 
-	private String getClientIdFromLocal() {
-		String clientId = null;
-		try {
-			byte[] data = getDataFromLocal(CLIENT_ID_FILENAME);
-			clientId = new String(data, "UTF-8");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+  private String getClientIdFromLocal() {
+    String clientId = null;
+    try {
+      byte[] data = getDataFromLocal(CLIENT_ID_FILENAME);
+      clientId = new String(data, "UTF-8");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
-		return clientId;
-	}
+    return clientId;
+  }
 
-	private boolean hasClientId() {
-		File f = new File(CLIENT_ID_FILENAME);
-		return f.exists() && !f.isDirectory();
-	}
+  private boolean hasClientId() {
+    File f = new File(CLIENT_ID_FILENAME);
+    return f.exists() && !f.isDirectory();
+  }
 
-	private String getChecksum(String filename) {
-		String checksum = null;
-		try {
-			byte[] data = getDataFromLocal(filename);
-			if (data != null) {
-				checksum = Utils.toMd5(data);
-			}
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
+  private String calculateChecksum(String filename) {
+    String checksum = null;
+    try {
+      byte[] data = getDataFromLocal(filename);
+      if (data != null) {
+        checksum = Utils.toMd5(data);
+      }
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
 
-		return checksum;
-	}
+    return checksum;
+  }
 }
--- code_tp1_2/src/ca/polymtl/inf4402/tp1/server/Server.java
+++ code_tp1_2/src/ca/polymtl/inf4402/tp1/server/Server.java
@@ -8,152 +8,145 @@ import java.rmi.server.UnicastRemoteObject;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
-
 import ca.polymtl.inf4402.tp1.shared.ServerInterface;
 import ca.polymtl.inf4402.tp1.shared.Utils;
 
 public class Server implements ServerInterface {
-	private Map<String, FilePoly> fileMap = new HashMap<String, FilePoly>();
-	private final byte[] EMPTY_FILE = new byte[0];
-	private static int clientId = 0;
+  private Map<String, FilePoly> fileMap = new HashMap<String, FilePoly>();
+  private final byte[] EMPTY_FILE = new byte[0];
+  private static int clientId = 0;
 
-	public static void main(String[] args) {
-		Server server = new Server();
-		server.run();
-	}
+  public static void main(String[] args) {
+    Server server = new Server();
+    server.run();
+  }
 
-	public Server() {
-		super();
-	}
+  public Server() {
+    super();
+  }
 
-	private void run() {
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
-		}
+  private void run() {
+    if (System.getSecurityManager() == null) {
+      System.setSecurityManager(new SecurityManager());
+    }
 
-		try {
-			ServerInterface stub = (ServerInterface) UnicastRemoteObject.exportObject(this, 0);
+    try {
+      ServerInterface stub = (ServerInterface) UnicastRemoteObject.exportObject(this, 0);
 
-			Registry registry = LocateRegistry.getRegistry();
-			registry.rebind("server", stub);
-			System.out.println("Server ready.");
+      Registry registry = LocateRegistry.getRegistry();
+      registry.rebind("server", stub);
+      System.out.println("Server ready.");
 
-		} catch (ConnectException e) {
-			System.err
-			    .println("Impossible de se connecter au registre RMI. Est-ce que rmiregistry est lancé ?");
-			System.err.println();
-			System.err.println("Erreur: " + e.getMessage());
-		} catch (Exception e) {
-			System.err.println("Erreur: " + e.getMessage());
-		}
-	}
+    } catch (ConnectException e) {
+      System.err.println("Impossible de se connecter au registre RMI. Est-ce que rmiregistry est lancé ?");
+      System.err.println();
+      System.err.println("Erreur: " + e.getMessage());
+    } catch (Exception e) {
+      System.err.println("Erreur: " + e.getMessage());
+    }
+  }
 
-	@Override
-	public String generateClientId() {
-		return "Client # " + ++clientId;
-	}
+  @Override
+  public String generateClientId() {
+    return "Client # " + ++clientId;
+  }
 
-	@Override
-	public String create(String filename) {
-		if (fileMap.containsKey(filename)) {
-			return filename + " existe déjà";
-		}
-		try {
-			createInMemory(filename, EMPTY_FILE, null);
-		} catch (NoSuchAlgorithmException e) {
-			return "Erreur lors de la création du fichier " + filename;
-		}
+  @Override
+  public String create(String filename) {
+    if (fileMap.containsKey(filename)) {
+      return filename + " existe déjà";
+    }
+    try {
+      createInMemory(filename, EMPTY_FILE, null);
+    } catch (NoSuchAlgorithmException e) {
+      return "Erreur lors de la création du fichier " + filename;
+    }
 
-		return filename + " à été ajouté";
-	}
+    return filename + " à été ajouté";
+  }
 
-	private void createInMemory(String filename, byte[] data, String clientId)
-	    throws NoSuchAlgorithmException {
-		String checksum = Utils.toMd5(data);
-		fileMap.put(filename, new FilePoly(filename, clientId, checksum, data));
-	}
+  private void createInMemory(String filename, byte[] data, String clientId) throws NoSuchAlgorithmException {
+    String checksum = Utils.toMd5(data);
+    fileMap.put(filename, new FilePoly(filename, clientId, checksum, data));
+  }
 
-	/**
-	 * TODO ajouter description du tp
-	 * @return Si le checksum est different, retourne le data, sinon null;
-	 */
-	@Override
-	public byte[] lock(String filename, String clientId, String checksum) {
-		FilePoly fileToRetrieve = fileMap.get(filename);
+  /**
+   * 
+   * @return Si le checksum est different, retourne le nouveau data, sinon null;
+   */
+  @Override
+  public byte[] lock(String filename, String clientId, String checksum) {
+    FilePoly fileToRetrieve = fileMap.get(filename);
 
-		if (fileToRetrieve == null) {
-			throw new IllegalArgumentException("Le fichier " + filename + " n'existe pas.");
-		}
+    if (fileToRetrieve == null) {
+      throw new IllegalArgumentException("Le fichier " + filename + " n'existe pas.");
+    }
 
-		if (fileToRetrieve.isLocked()) {
-			throw new IllegalArgumentException(filename + " est déjà verrouillé par "
-			    + fileToRetrieve.getClientId());
-		}
+    if (fileToRetrieve.isLocked()) {
+      throw new IllegalArgumentException(filename + " est déjà verrouillé par " + fileToRetrieve.getClientId());
+    }
 
-		fileToRetrieve.setClientId(clientId);
-		
-		
+    fileToRetrieve.setClientId(clientId);
 
-		return fileToRetrieve.getChecksum().equals(checksum) ? null : fileToRetrieve.getData();
-	}
+    return fileToRetrieve.getChecksum().equals(checksum) ? null : fileToRetrieve.getData();
+  }
 
-	@Override
-	public byte[] get(String filename, String checksum) {
-		FilePoly fileToRetrieve = fileMap.get(filename);
+  @Override
+  public byte[] get(String filename, String checksum) {
+    FilePoly fileToRetrieve = fileMap.get(filename);
 
-		if (fileToRetrieve == null) {
-			throw new IllegalArgumentException("Le fichier " + filename + " n'existe pas.");
-		}
+    if (fileToRetrieve == null) {
+      throw new IllegalArgumentException("Le fichier " + filename + " n'existe pas.");
+    }
 
-		if (fileToRetrieve.getChecksum().equals(checksum)) {
-			return null;
-		}
+    if (fileToRetrieve.getChecksum().equals(checksum)) {
+      return null;
+    }
 
-		return fileToRetrieve.getData();
-	}
+    return fileToRetrieve.getData();
+  }
 
-	@Override
-	public String list() {
+  @Override
+  public String list() {
 
-		StringBuilder output = new StringBuilder();
+    StringBuilder output = new StringBuilder();
 
-		for (FilePoly file : fileMap.values()) {
-			String clientId = file.getClientId() != null ? " verrouillé par " + file.getClientId()
-			    : " non vérrouillé";
-			output.append(file.getFilename()).append(clientId).append("\n");
-		}
+    for (FilePoly file : fileMap.values()) {
+      String clientId = file.getClientId() != null ? " verrouillé par " + file.getClientId() : " non vérrouillé";
+      output.append(file.getFilename()).append(clientId).append("\n");
+    }
 
-		return output.append(fileMap.values().size()).append(" fichier(s)").toString();
-	}
+    return output.append(fileMap.values().size()).append(" fichier(s)").toString();
+  }
 
-	@Override
-	public String push(String filename, byte[] data, String clientId) {
-		FilePoly fileToRetrieve = fileMap.get(filename);
+  @Override
+  public String push(String filename, byte[] data, String clientId) {
+    FilePoly fileToRetrieve = fileMap.get(filename);
 
-		if (fileToRetrieve == null || !fileToRetrieve.isLocked() || !fileToRetrieve.getClientId().equals(clientId)) {
-			return filename + " doit être verrouillé";
-		}
+    if (fileToRetrieve == null || !fileToRetrieve.isLocked() || !fileToRetrieve.getClientId().equals(clientId)) {
+      return filename + " doit être verrouillé";
+    }
 
-		try {
-			createInMemory(filename, data, clientId);
-			fileToRetrieve.setClientId(null);
-			fileMap.put(filename, fileToRetrieve);
-		} catch (NoSuchAlgorithmException e) {
-			return "Erreur lors de la création du fichier " + filename;
-		}
-		return filename + " a été envoyé au serveur";
-	}
+    try {
+      createInMemory(filename, data, clientId);
+      fileToRetrieve.setClientId(null);
+      fileMap.put(filename, fileToRetrieve);
+    } catch (NoSuchAlgorithmException e) {
+      return "Erreur lors de la création du fichier " + filename;
+    }
+    return filename + " a été envoyé au serveur";
+  }
 
-	@Override
-	public String[][] syncLocalDir() throws RemoteException {
-		String[][] files = new String[fileMap.values().size()][2];
-		
-		int i = 0;
-		for (FilePoly file : fileMap.values()) {
-			files[i] = file.getExportData();
-			i++;
-		}
-		
-		return files;
-	}
+  @Override
+  public String[][] syncLocalDir() throws RemoteException {
+    String[][] files = new String[fileMap.values().size()][2];
+
+    int i = 0;
+    for (FilePoly file : fileMap.values()) {
+      files[i] = file.getExportData();
+      i++;
+    }
+
+    return files;
+  }
 }
